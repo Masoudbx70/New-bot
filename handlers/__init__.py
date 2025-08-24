f2715131f1f9819dee40a8e94f6b92b4fdd5ebf1
Masoudbx70 <62997872+Masoudbx70@users.noreply.github.com>
2025-08-24 22:40:20 +0330

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,13 +1,4 @@
 from .group_handlers import welcome_new_member, goodbye_member, handle_group_message
 from .auth_handlers import start, start_verification, handle_name, handle_phone, handle_screenshot1, handle_screenshot2, cancel, handle_callback_query
-from .admin_handlers import admin_panel, members_list, pending_requests, manage_guide_images, bot_stats, handle_admin_callback, verify_user, reject_user
+from .admin_handlers import admin_panel, members_list, pending_requests, manage_guide_images, bot_stats, verify_user, reject_user, callback_query_handler
 from .support_handlers import help_command, support, forward_to_support, handle_contact
-
-__all__ = [
-    'welcome_new_member', 'goodbye_member', 'handle_group_message',
-    'start', 'start_verification', 'handle_name', 'handle_phone', 
-    'handle_screenshot1', 'handle_screenshot2', 'cancel', 'handle_callback_query',
-    'admin_panel', 'members_list', 'pending_requests', 'manage_guide_images', 
-    'bot_stats', 'handle_admin_callback', 'verify_user', 'reject_user',
-    'help_command', 'support', 'forward_to_support', 'handle_contact'
-]
